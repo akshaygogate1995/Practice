4a7e6ad03df19e608c864feca9f0af9c57258384
akshaygogate1995 <123295575+akshaygogate1995@users.noreply.github.com>
2023-09-13 09:54:43 +0530

    Update checkuser.sh

--- checkuser.sh
+++ checkuser.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 
+# Name - Akshay Gogate
+# Purpose - To check if user exist in Linux Server
+
+read -p " Please enter your username to check if you exist in linux server - " USERNAME
+
+grep $USERNAME /etc/passwd
+
+if [ $? -eq 0 ]
+then
+        echo " Hello $USERNAME, welcome to linux server"
+else
+        echo " Sorry you are not allowed"
+fi
+
+
