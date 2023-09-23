ec84e3ed8dc503b6383c41d6e09e1c3059913bfe
akshaygogate1995 <123295575+akshaygogate1995@users.noreply.github.com>
2023-09-23 11:35:31 +0530

    Update print2to20.sh

--- print2to20.sh
+++ print2to20.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# Using a for loop to print even numbers from 2 to 20
+# Name - akshay gogate
+# date - 21/09/2023
+# email - akshay.gogate@hotmail.com
+
+
+# Using a for loop to print even numbers from 2 to 20 - 
 
 for ((i = 2; i <= 20; i += 2)); do
     echo "$i"
