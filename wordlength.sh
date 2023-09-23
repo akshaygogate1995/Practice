757c749c6875d69b3f482e3544a54e73e901ab0b
akshaygogate1995 <123295575+akshaygogate1995@users.noreply.github.com>
2023-09-23 11:33:59 +0530

    Update wordlength.sh

--- wordlength.sh
+++ wordlength.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Name - akshay gogate
+# date - 21/09/2023
+# email - akshay.gogate@hotmail.com
+
 # Specify the directory containing text files
-directory="/path/to/your/directory"
+directory="(/path/to/your/directory)"
 
 # Check if the directory exists
 if [ ! -d "$directory" ]; then
@@ -9,9 +13,10 @@ if [ ! -d "$directory" ]; then
     exit 1
 fi
 
-# Loop through text files in the directory
+# Loop through .txt files in the provided directory
 for file in "$directory"/*.txt; do
-    # Check if the file is readable
+
+    # Checking if the file is readable
     if [ -r "$file" ]; then
         echo "Processing file: $file"
 
