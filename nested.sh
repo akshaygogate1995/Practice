0760247adc0f437d96592e159b0158e64f9c6794
akshaygogate1995 <123295575+akshaygogate1995@users.noreply.github.com>
2023-09-23 11:36:33 +0530

    Update nested.sh

--- nested.sh
+++ nested.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# Name - akshay gogate
+# date - 21/09/2023
+# email - akshay.gogate@hotmail.com
+
+
 # Define the number of rows for the pattern
+
 rows=5
 
-# Outer loop for the number of rows
+# Loop for the number of rows
+
 for ((i = 1; i <= rows; i++)); do
-    # Inner loop to print numbers from 1 to i
+    # Internal loop to print numbers from 1 to i
     for ((j = 1; j <= i; j++)); do
         echo -n "$j"
     done
